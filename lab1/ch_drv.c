2fa93e7f519b0820134773eff05641a5e6d0ec55
danya <daniil.afanaev.2018@mail.ru>
2024-02-28 14:51:34 +0300

    done

--- lab1/ch_drv.c
+++ lab1/ch_drv.c
@@ -6,8 +6,10 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/slab.h>
 
-#define BUF_SIZE 256
+#define BUF_SIZE 2048
+#define PARSE_ERROR 5
 
 static DEFINE_MUTEX(mutex);
 
@@ -15,16 +17,126 @@ static dev_t first;
 static struct cdev c_dev;
 static struct class * cl;
 
-char ibuf[BUF_SIZE];
-int counter = 0;
+struct list_res {
+    struct list_res *head;
+    struct list_res *last;
+    struct list_res *prev;
+    struct list_res *next;
+    long result;
+    long id;
+};
 
-int check_cond(char symbol) {
-    return symbol != '\n' && symbol != '\r' ? 1 : 0;
+struct list_res *head;
+
+char ibuf[BUF_SIZE];
+int id_counter = 1;
+
+int my_stoi(int *n, char *s) {
+    const int size_s = strlen(s);
+
+    int sign = 0;
+    int i = 0;
+    if (s[0] == '+')
+        i = 1;
+    if (s[0] == '-') {
+        i = 1;
+        sign = 1;
+    }
+
+    int value = 0;
+    for (; i < size_s; ++i) {
+        if (s[i] < '0' || s[i] > '9') {
+            return 0;
+        }
+        int digit = s[i] - '0';
+        value = value * 10 + digit;
+    }
+
+    if (sign == 1)
+        value = value * -1;
+
+    *n = value;
+    return 0;
+}
+
+int is_num(char symbol) {
+    return symbol >= '0' && symbol <= '9' ? 1 : 0;
+}
+
+int is_char(char symbol) {
+    return ((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z')) ? 1 : 0;
+}
+
+int is_action(char symbol) {
+    return (symbol == '&' || symbol == '|' || symbol == '^') ? 1 : 0;
+}
+
+int get_action(char action) {
+    if (action == '&') return 0;
+    if (action == '|') return 1;
+    if (action == '^') return 2;
+    return -1;
+}
+
+int calculate(int val1, int val2, char action) {
+    int result;
+    int act = get_action(action);
+
+    if (act == 0) {
+        result = val1 & val2;
+    } else if (act == 1) {
+        result = val1 | val2;
+    } else if (act == 2) {
+        result = val1 ^ val2;
+    } else {
+        result = -1;
+    }
+
+    return result;
 }
 
 void clear_buffer(void) {
     for (int i = 0; i < BUF_SIZE; i++) {
         ibuf[i] = '\x00';
+    }
+}
+
+void clear_subbuffer(char *buf) {
+    for (int i = 0; i < strlen(buf); i++) {
+        if (!is_num(buf[i])) buf[i] = '\x00';
+    }
+}
+
+void get_message_error(void) {
+    clear_buffer();
+    ibuf[0] = 'E';
+    ibuf[1] = 'r';
+    ibuf[2] = 'r';
+    ibuf[3] = 'o';
+    ibuf[4] = 'r';
+}
+
+void get_text_result(struct list_res *res, char *resp) {
+
+    char *curr = kcalloc(32, sizeof(char), GFP_KERNEL);
+
+    printk(KERN_INFO "Val2: %d", res->result);
+
+    snprintf(curr, 32, "id = %ld: result = %ld\n", res->id, res->result);
+
+    strcat(resp, curr);
+    kfree(curr);
+}
+
+void get_text_list_result(struct list_res *list, char *resp) {
+
+    if (list != NULL) {
+        struct list_res *next;
+        while (list != NULL) {
+            next = list->prev;
+            get_text_result(list, resp);
+            list = next;
+        }
     }
 }
 
@@ -59,7 +171,6 @@ static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off
     *off = count;
 
     printk(KERN_INFO "Driver: finish reading\n");
-    printk(KERN_INFO "Symbols counter: %d", counter);
     return count;
 }
 
@@ -76,14 +187,81 @@ static ssize_t my_write(struct file *f, const char __user *buf,  size_t len, lof
          return -EFAULT;
     }
 
+    char action;
+    char v1c[34], v2c[34];
+    int flag = 0, length_val1 = 0, length_val2 = 0;
+
     for (int i = 0; i < sizeof(ibuf); i++) {
         char symbol = ibuf[i];
-        if (symbol == '\x00') break;
-        //printk(KERN_INFO "Symbol: %c", ibuf[i]);
-        if (check_cond(symbol)) {
-            counter++;
+
+        if (symbol == '\x00') {
+            break;
+        }
+
+        if (symbol == ' ') {
+            continue;
+        }
+
+        if (is_char(symbol)) {
+            get_message_error();
+            return PARSE_ERROR;
+        }
+
+        if (is_action(symbol)) {
+            action = symbol;
+            flag = 1;
+        }
+
+        if (is_num(symbol)) {
+            if (flag == 0) {
+                v1c[length_val1++] = symbol;
+            } else {
+                v2c[length_val2++] = symbol;
+            }
         }
     }
+
+    clear_subbuffer(v1c);
+    clear_subbuffer(v2c);
+
+    int val1 = 0, val2 = 0;
+
+    my_stoi(&val1, v1c);
+    my_stoi(&val2, v2c);
+
+    int result = calculate(val1, val2, action);
+
+    if (id_counter == 1) {
+        head = kmalloc(sizeof(struct list_res), GFP_KERNEL);
+
+        head->id = id_counter++;
+        head->result = result;
+
+        head->prev = NULL;
+        head->next = NULL;
+        head->head = head;
+        head->last = head;
+
+    } else {
+        struct list_res *curr = kmalloc(sizeof(struct list_res), GFP_KERNEL);
+
+        curr->id = id_counter++;
+        curr->result = result;
+
+        curr->head = head->head;
+
+        curr->prev = head->last;
+        curr->next = NULL;
+
+        head->last = curr;
+    }
+
+
+    char *resp = kcalloc(BUF_SIZE, sizeof(char), GFP_KERNEL);
+
+    get_text_list_result(head->last, resp);
+
+    sprintf(ibuf, "%s\n", resp);
 
     printk(KERN_INFO "Driver: finish writing()\n");
     return len;
